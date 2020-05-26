cd4d7b88f3be9f8962195022c66eee6ba55dc0ce
piotr piwonski <piwonski@student.agh.edu.pl>
2020-05-26 14:31:41 +0200

    bedny opis

--- Class.py
+++
@@ -1,9 +0,0 @@
-class Events:
-    def __init__(self, name_event, place_event, date_event):
-        self.name_event = name_event
-        self.place_event = place_event
-        self.date_event = date_event
-
-#lista służąca to przechowywania elementów stworzonych obiektów klasy
-event = []
-
