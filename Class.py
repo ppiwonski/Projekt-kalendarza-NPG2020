c0e76ca7b7b0b67704d86b976507677b5687e243
piotr piwonski <piwonski@student.agh.edu.pl>
2020-06-03 22:15:04 +0200

    plik z klasa obslugujaca wydarzenia

--- Class.py
+++ Class.py
@@ -1,8 +1,19 @@
+import datetime,math
 class Events:
     def __init__(self, name_event, place_event, date_event):
         self.name_event = name_event
         self.place_event = place_event
         self.date_event = date_event
+
+    def CountdownDays(self):
+        today = datetime.date.today()
+        difference = (today-self.date_event).days
+        if difference > 0:
+            return f'Wydarzenie odbyło się {int(difference)} dni temu.'
+        elif difference < 0:
+            return f'Wydarzenie odbędzie się za {int(math.fabs(difference))} dni.'
+        else:
+            return f'Wydarzenie odbywa się dzisiaj.'
 
 #lista służąca to przechowywania elementów stworzonych obiektów klasy
 event = []
