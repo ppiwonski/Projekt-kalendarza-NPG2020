fcbe8e03246045510923a168b51adbaae43241e3
KubaGrzyb <62983534+KubaGrzyb@users.noreply.github.com>
2020-06-25 16:10:23 +0200

    Dodatkowe funkcje - oczyszczanie ekranu po wykonanej operacji oraz funkcja: Aby kontynuowac, nacisnij Enter

---
+++ AdditionalFunc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+def PressEnter():
+    x=input('\nAby kontynuować, naciśnij "Enter"...')
+    print("Proszę czekać...")
+
+def ClearScreen():
+    os.system('cls')
